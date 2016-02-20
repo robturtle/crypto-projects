3ef0893288db98c9f7633283fffcad5870676730
Yang Liu <jeremyrobturtle@gmail.com>
2016-02-20 03:42:29 -0500

    test base facilities

--- project1/test/KeyTest.cpp
+++ project1/test/KeyTest.cpp
@@ -1,14 +1,115 @@
 #include "gtest/gtest.h"
+#include "cppqc.h"
 #include "Key.hpp"
 #include <numeric>
+#include <random>
+#include <algorithm>
 using namespace std;
 using namespace cipher;
+using namespace cppqc;
 
-TEST (KeyTest, AlphabetRelatedSize) {
+/********************************************************************************
+* Fixture
+********************************************************************************/
+static array<int, KEY_LEN> codes;
+auto engine = default_random_engine{};
+
+class KeyTest: public ::testing::Test {
+protected:
+  void SetUp() {
+    for (size_t i = 0; i < KEY_LEN; i++) codes[i] = i;
+  }
+};
+
+
+/********************************************************************************
+* Basic Pre-conditions
+********************************************************************************/
+TEST_F (KeyTest, AlphabetRelatedSize) {
   EXPECT_EQ(alphabet_len, sizeof(slots) / sizeof(int));
   EXPECT_EQ(alphabet_len, base.size());
 }
 
-TEST (KeyTest, FrequencySumUpTo_KEY_LEN) {
-  EXPECT_EQ(KEY_LEN, accumulate(begin(slots), end(slots), 0));
+TEST_F (KeyTest, FrequencySumUpTo_KEY_LEN) {
+  EXPECT_EQ((int)KEY_LEN, accumulate(begin(slots), end(slots), 0));
 }
+
+
+/********************************************************************************
+* Generator of Key
+********************************************************************************/
+namespace cipher {
+  ostream& operator<<(ostream &o, const Key &k) {
+    char ch = alpha;
+    int idx = 0;
+    for (int slot : slots) {
+      o << ch++ << ": ";
+      auto key = k.key();
+      for (int i = 0; i < slot; i++) {
+        o << key[idx++] << ", ";
+      }
+      o << "\n";
+    }
+    return o;
+  }
+}
+
+template <>
+struct ArbitraryImpl<Key> {
+  static const typename Arbitrary<Key>::unGenType unGen;
+  static const typename Arbitrary<Key>::shrinkType shrink;
+};
+
+const typename Arbitrary<Key>::unGenType ArbitraryImpl<Key>::unGen = [](RngEngine &, size_t) {
+  array<int, KEY_LEN> copied = codes;
+  shuffle(begin(copied), end(copied), engine);
+  return Key(copied);
+};
+
+const typename Arbitrary<Key>::shrinkType ArbitraryImpl<Key>::shrink = [](Key){
+  return vector<Key>();
+};
+
+
+/********************************************************************************
+* Properties
+********************************************************************************/
+#define RUN_QUICK_CHECK(property) TEST_F (KeyTest, property) \
+  {EXPECT_EQ(cppqc::QC_SUCCESS, cppqc::quickCheckOutput(property()).result);}
+
+struct KeyIsAPermutation: Property<Key> {
+  bool check(const Key &k) const override {
+    array<int, KEY_LEN> copied = k.key();
+    sort(begin(copied), end(copied));
+    return codes == copied;
+  }
+};
+RUN_QUICK_CHECK(KeyIsAPermutation)
+
+struct KeyDistNotBias: Property<vector<Key>> {
+  bool check(const vector<Key> &keys) const override {
+    for (auto i = begin(keys); i < end(keys); ++i) {
+      for (auto j = i + 1; j < end(keys); ++j) {
+        if ((*i).key() == (*j).key()) return false;
+      }
+    }
+    return true;
+  }
+};
+RUN_QUICK_CHECK(KeyDistNotBias)
+
+struct KeySubIndexCoversAllKeys: Property<Key> {
+  bool check(const Key& k) const override {
+    array<int, KEY_LEN> touched;
+    int idx = 0;
+    for (char c = alpha; c <= omega; c++) {
+      int slot_num = slots[c - alpha];
+      for (int i = 0; i < slot_num; i++) {
+        touched[idx++] = k.code(c, i);
+      }
+    }
+    sort(begin(touched), end(touched));
+    return touched == codes;
+  }
+};
+RUN_QUICK_CHECK(KeySubIndexCoversAllKeys)
---
+++ project1/test/testing.hpp
@@ -0,0 +1,30 @@
+#ifndef GTEST_QUICKCHECK_COORPERATE_YANG_LIU_0211
+#define GTEST_QUICKCHECK_COORPERATE_YANG_LIU_0211
+#include "gtest/gtest.h"
+#include "cppqc.h"
+#include <vector>
+
+#define REGISTER_ARBITRARY(Class, gen, shrinker)      \
+  template <> struct cppqc::ArbitraryImpl<Class> { \
+    static const typename cppqc::Arbitrary<Class>::unGenType unGen;  \
+    static const typename cppqc::Arbitrary<Class>::shrinkType shrink;  \
+  };\
+  const typename cppqc::Arbitrary<Class>::unGenType \
+  cppqc::ArbitraryImpl<Class>::unGen = [](cppqc::RngEngine &rng, std::size_t size) { \
+    return gen(rng, size);\
+  }; \
+  const typename cppqc::Arbitrary<Class>::shrinkType \
+  cppqc::ArbitraryImpl<Class>::shrink = [](Class c) { \
+    return shrinker(c);\
+  }
+
+
+// wrap a quickcheck runner into a gtest test case
+#define RUN_QUICK_CHECK(fixture, property) TEST (fixture, property)    \
+  {EXPECT_EQ(cppqc::QC_SUCCESS, cppqc::quickCheckOutput(property()).result);}
+
+
+#define RUN_QUICK_CHECK_F(fixture, property) TEST_F (fixture, property)    \
+  {EXPECT_EQ(cppqc::QC_SUCCESS, cppqc::quickCheckOutput(property()).result);}
+
+#endif /* GTEST_QUICKCHECK_COORPERATE_YANG_LIU_021 */
